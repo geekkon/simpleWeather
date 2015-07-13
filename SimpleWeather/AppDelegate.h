56fbc129c8aca72b88e4e0ce61e3f0ea5bad97a7
Dim <dim@MacBook-Pro-Dim.local>
2015-07-14 02:06:54 +0400

    Initial Commit

---
+++ SimpleWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by Dim on 14.07.15.
+//  Copyright (c) 2015 Dmitriy Baklanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleWeather
+//
+//  Created by Dim on 14.07.15.
+//  Copyright (c) 2015 Dmitriy Baklanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
